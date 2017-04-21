5a5d51bb283a0c5ea96bba522a450e6d8fef2e99
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-21 16:14:22 -0500

    Update Install_Powershell_Kali.sh

--- Install_Powershell_Kali.sh
+++ Install_Powershell_Kali.sh
@@ -6,6 +6,8 @@ dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.18/powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
 dpkg -i powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
 apt-get install -f
+rm powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
+rm libicu55_55.1-7ubuntu0.1_amd64.deb
 
 #standalone powershell binary
 #curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
